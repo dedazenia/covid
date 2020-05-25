3663575ba3b897c380c7577ffc39f5eb6a9663ff
Evgeny Gorobets <egorobets@gmail.com>
2020-05-25 13:23:46 +0300

    csv parsing with Apache Commons CSV starts

--- src/main/java/ru/gorobets24/covid/services/CovidDataService.java
+++ src/main/java/ru/gorobets24/covid/services/CovidDataService.java
@@ -1,16 +1,23 @@
 package ru.gorobets24.covid.services;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
 import java.io.IOException;
+import java.io.StringReader;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
-
+@Service
 public class CovidDataService {
 
-    private static String COVID_DATA_URL = "https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+    private static String COVID_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
 
+    @PostConstruct
     public void extractCovidData() throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
@@ -19,6 +26,13 @@ public class CovidDataService {
         HttpResponse<String> httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
         System.out.println(httpResponse.body());
 
+        StringReader csvReader = new StringReader(httpResponse.body());
+        Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvReader);
+        for (CSVRecord record : records) {
+            String provinceState = record.get("Province/State");
+            System.out.println(provinceState);
 
+
+        }
     }
 }
