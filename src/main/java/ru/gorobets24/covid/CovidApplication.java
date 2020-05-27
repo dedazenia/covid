2289fb91fb4c60a0cac7ebb61a1fac31acc29469
Evgeny Gorobets <egorobets@gmail.com>
2020-05-27 14:54:45 +0300

    added local js sorting scr added addResourceHandlers into CovidApplication.java

--- src/main/java/ru/gorobets24/covid/CovidApplication.java
+++ src/main/java/ru/gorobets24/covid/CovidApplication.java
@@ -3,13 +3,19 @@ package ru.gorobets24.covid;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableScheduling
-public class CovidApplication {
+public class CovidApplication implements WebMvcConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(CovidApplication.class, args);
     }
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static.js/**").addResourceLocations("classpath:/static.js/");
+    }
 }
